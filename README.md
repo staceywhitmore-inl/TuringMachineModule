689ba69a28c5c40993c1ddd79feedb32dd9d62d5
staceywhitmore-inl <51754579+staceywhitmore-inl@users.noreply.github.com>
2022-10-12 11:32:51 -0600

    All functionality has been added and README.md updated with a better description. Debugging output has been commented out and some optimizations added to reduce storage.

--- README.md
+++ README.md
@@ -5,6 +5,24 @@ An embedded software implementation of a Turing Machine module for a Eurorack sy
 
 [... and a video explaing how they work](https://www.youtube.com/watch?v=va2XAdFtmeU&feature=emb_imp_woyt&themeRefresh=1)
 
+This started out as an adaptation of the LightRider sketch in Jeremy Blum's "Exploring Arduino"
+and then I thought to myself, "I could make musical sequence with this.."
+It turns out somebody alread has. What I've built here is know by modular synthesizer enthusiasts as a Turing Machine Module.
+A better explanation of what they do and how they work can be found here: 
+https://www.thonk.co.uk/shop/turingmkii/
 
+I thought to take this rite of passage myself and I've modified the aforementioned LightRider LED sketch to take an external clock signal (for speed control)
+and to produce a voltage via an MCP4921 DAC by mapping current 8-bit value in shift registers (SN74LS14N shift register IC)
+to 12-bit value passed to DAC and interpolated to millivolt increments.
+This volts-per-octave signal can then be fed to a Voltage controlled  oscillator to generate notes.
+Of course, they are random and not musical (unless ran through a quantizer to assign them to the closest note in a given musical scale. [This will be a separate project])
 
+8v / 256 = 0.03125 0r 0.033 or 33 milliVolts
+multiply the 8-bit # the LEDs represent by 0.033 to get the volts/octave output
+All LEDs lit would add up to ~ 8.4 volts; however, because the MCP4921 is only rated for a maximum 5v VCC (which it uses as a reference voltage) 
+the max voltage output is just under 5VDC. (I later plan to transition this project from being more software-oriented to more of a hardware implementation 
+(e.g., more similar to the DIY kits mentioned in the video above) which will use a DAC0800L CN chip cable of reaching the upper octaves [but those higher octaves are shrill so this shoudl do for now.])
+I will add a voltage divider circuit (with an adjustable trim pot) at the DAC's output to further attenuate the voltage and limit the output to lower notes if desired.
 
+Because I do have the needed DPDT, 3-way switch for manually assigning 1 or 0 bits to the sequence, I've created a software implementation that utizies two momentary tactile buttons
+for this purpose and 10K trim pot to adjust the probability of 'randomly' inverting the carried bit. 
--- TuringMachineModule.cpp
+++ TuringMachineModule.cpp
@@ -1,248 +1,227 @@
 //#include <TimerOne.h> // Can be used for SOFTware interrupts to call a f(n) at an interval set in microSeconds Use for Gate Length (Software Interrupt)
 #include <SPI.h>
 #include "MCP_DAC.h" // reference library for MCP4921 DAC IC
-MCP4921 DAC;       // create DAC object of type MCP4921 
+MCP4921 DAC;         // create DAC object of type MCP4921
 
-// This is an Adaptation of the LightRider sketch in Jeremy Blum's "Exploring Arduino" 
-// which I've modified to take an external clock signal (for speed control) 
+// This started out as an adaptation of the LightRider sketch in Jeremy Blum's "Exploring Arduino"
+// and then I thought to myself, "I could make musical sequence with this.."
+// It turns out somebody alread has. What I've built here is know by modular synthesizer enthusiasts as a Turing Machine Module.
+// A better explanation of what they do and how they work can be found here: 
+// https://www.thonk.co.uk/shop/turingmkii/
+
+// I thought to take this rite of passage myself and I've modified the aforementioned LightRider LED sketch to take an external clock signal (for speed control)
 // and to produce a voltage via an MCP4921 DAC by mapping current 8-bit value in shift registers (SN74LS14N shift register IC)
-// to 12-bit value passed to DAC and interpolated to millivolt increments 
+// to 12-bit value passed to DAC and interpolated to millivolt increments.
+// This volts-per-octave signal can then be fed to a Voltage controlled  oscillator to generate notes.
+// Of course, they are random and not musical (unless ran through a quantizer to assign them to the closest note in a given musical scale. [This will be a separate project])
 
 /*
-8v / 256 = 0.03125 0r 0.033 or 33 milliVolts 
+8v / 256 = 0.03125 0r 0.033 or 33 milliVolts
 multiply the 8-bit # the LEDs represent by 0.033 to get the volts/octave output
-All LEDs lit add up to ~ 8.4 volts 
+All LEDs lit would add up to ~ 8.4 volts; however, because the MCP4921 is only rated for a maximum 5v VCC (which it uses as a reference voltage) 
+the max voltage output is just under 5VDC. (I later plan to transition this project from being more software-oriented to more of a hardware implementation 
+(e.g., more similar to the DIY kits mentioned in the video above) which will use a DAC0800L CN chip cable of reaching the upper octaves [but those higher octaves are shrill so this shoudl do for now.])
+I will add a voltage divider circuit (with an adjustable trim pot) at the DAC's output to further attenuate the voltage and limit the output to lower notes if desired.
+
+Because I do have the needed DPDT, 3-way switch for manually assigning 1 or 0 bits to the sequence, I've created a software implementation that utizies two momentary tactile buttons
+for this purpose and 10K trim pot to adjust the probability of 'randomly' inverting the carried bit. 
+
+
 */
 
-const int CLK_PIN_IN = 0; // Interrupt 0 (pin 2 on Uno) for incoming clock source (Arturia Key Step sequencer "sync out" jack).
+// Arduino pins   | DAC pins
+//----------------|-----------: -----------------------------------------------
+// Pin D10 (SS)     <--  CS(2): Pin 10 declared in setup() by "DAC.begin(10);"
+// pin D13 (SCLCK)  <-- SCK(3): Declared by Default by Library  (SDI = Serial Data Interface) (*Note, I twisted these 2 wires on wire harness (yellow&orange wires)
+// Pin D11 (MOSI)   <-- SDI(4): Declared by Default by Library  (MOSI = Master Out Slave In)  (*)
 
- // Arduino pins   | DAC pins
- //----------------|-----------: -----------------------------------------------
- // Pin D10 (SS)     <--  CS(2): Pin 10 declared in setup() by "DAC.begin(10);"    
- // Pin D11 (MOSI)   <-- SCK(3): Declared by Default by Library  
- // pin D13 (SCLCK)  <-- SCK(3): Declared by Default by Library  
+// const uint32_t GATE_LENGTH = 500000; // .5 Sec = 1/2 million microseconds (500,000 uS)
 
+const int CLK_INTRPT_PIN = 0; // Interrupt 0 (I.e., PIN 2) (actually uses pin 2 on Uno) for incoming clock source (Arturia Key Step sequencer "sync out" jack).
 
-//const uint32_t GATE_LENGTH = 500000; // .5 Sec = 1/2 million microseconds (500,000 uS)
-// Button, Pot and Read INPUT pins
-const int BTN_HIGH           = 2;
-const int BTN_LOW            = 7;
-const int RND_POT            = 0; // AO
-const int OUT_BIT_READ_PIN   = 12; //D12
+// Declare SHIFT REGISTER Pins
+const int SER_DATA_PIN = 4;
+const int LATCH_PIN = 5;
+const int CLK_PIN = 6;
 
-// Declare SHIFT REGister Pins
-const int SER_DATA_PIN  =4; // TODO: change to 4 (was 8)
-const int LATCH_PIN     =5; // "": " " 5 ~       (was 9)
-const int CLK_PIN       =6; // "": " " 6 ~       (was 10)
+// Other Input Pins
+const int BTN_HIGH = 7;
+const int BTN_LOW = 8;
+const int RND_POT = A1;          // [A]O used for random seed.
+const int OUT_BIT_READ_PIN = 12; // N The bit shifted out from shift register is sent to this pin on the Arduino.
 
-
-
-// Declare associated Interrupt variables
+// Declare (VOLATILE) associated Interrupt variables.
 volatile int cvOutput = 0;
-volatile int currentBitIndex = 0;
+// volatile int  currentIteration = 0;//TODO: REMOVE. No longer needed.  WAS currentBitIndex = 0;
 volatile int dataPinState;
+volatile byte currentByte;
+volatile int rndPotValue = 0;
+volatile int rndInt;
 
-// TEST Sequence of LEDs
-int currentValue = 212;
+const int NUMBER_OF_BITS_IN_SEQ = 8; // Const for now but would become assignable should a rotary selector is added later.
 
-const int NUMBER_OF_BITS = 8;
+void setup()
+{ 
+  //Serial.begin(230400);// Comment(ed) out when not displaying debugging messages to free up resources.
+  // Pin Setup For SHIFT REGISTER
+  pinMode(SER_DATA_PIN, OUTPUT); // 4
+  pinMode(LATCH_PIN, OUTPUT);    // 5
+  pinMode(CLK_PIN, OUTPUT);      // 6
+  //Button inputs
+  pinMode(OUT_BIT_READ_PIN, INPUT); // 12
+  pinMode(BTN_HIGH, INPUT);         // 7
+  pinMode(BTN_LOW, INPUT);          // 8
+  pinMode(RND_POT, INPUT);          // A1 (A0 used to seed random())
 
-void setup() {
- // Set Input Buttons (Not required) 
- pinMode(BTN_HIGH, INPUT); // 2
- pinMode(BTN_LOW, INPUT);  // 7 
-  //Setup for Interrupts
- Serial.begin(230400); // 9600
+  // DAC chip select (CS) signal comes from Arduino. Set Arduino SS out to pin D10
+  DAC.begin(10); // Initialize on Pin 10
 
-  // Setup For Shift Reg
- pinMode(SER_DATA_PIN, OUTPUT);
- pinMode(LATCH_PIN, OUTPUT);
- pinMode(CLK_PIN, OUTPUT);
+  attachInterrupt(CLK_INTRPT_PIN, externalClkReceived, RISING); // PIN 2 of Arduino UNO
 
- 
-// DAC chip select (CS) signal comes from Arduino. Set Arduino SS out to pin D10
- DAC.begin(10);     // Initialize on Pin 10 
+  // Ground LATCH_PIN and hold LOW for as long as transmitting
+  digitalWrite(LATCH_PIN, 0);
 
- // To make generated number more 'random', I seed it with noise generated an unassigned, floating analog pin. 
- randomSeed(analogRead(A5));
+  firstShift(SER_DATA_PIN, CLK_PIN, 0b00000000); // (4, 6, 0b10000001)
 
- attachInterrupt(CLK_PIN_IN, externalClkReceived, RISING);
+  // Return the latch pin HIGH to signal chip that it no longer needs to listen for information  (Output vals from shiftReg)
+  digitalWrite(LATCH_PIN, 1);
+  randomSeed(analogRead(0)); // Noise on analog pin A0 will cause randomSeed()to generate different seed numbers each time the program is run to make it 'more' "random".
 }
 
-
-// No loop is used 
-void loop() {
+// TODO: REMOVE WHEN NO LONGER NEEDED
+void loop()
+{
   //Serial.print("Current v/oct binary value is : ");
   //Serial.println(currentBitIndex);
 }
 
-
-
-// Interrupt f(n) 
-void externalClkReceived() {
-  changeVoltageBits();
-  
-  currentBitIndex++;
-  if (currentBitIndex >= NUMBER_OF_BITS){
-    currentBitIndex = 0;
-  }
-}
-
-// Timer Interrupt f(n)
-void changeVoltageBits() {
-  // 0.a) // Latch LOW to start sending
-   digitalWrite(LATCH_PIN, LOW); 
-   shiftOut(SER_DATA_PIN, CLK_PIN, currentValue); // Custom shiftBits to override built-in shiftBits() 
-   changeVoltage(currentValue);
-   digitalWrite(LATCH_PIN, HIGH);  // Latch HIGHT to SHOW the pattern (e.g. on the LEDs)   
-}
-
-
-
-// Called by Interrupt f(n): externalClkReceived
-void changeVoltage(int val) {
-  cvOutput = map(val, 0, 128, 0, 4095);
-  cvOutput= constrain(cvOutput, 0, 4095);
- // Serial.print("[");
- // Serial.print(currentBitIndex); Serial.print("] = "); Serial.print(val); Serial.print(" ---> ");  Serial.println(cvOutput);
-  DAC.analogWrite(cvOutput);   
-}
-
-
-
-
-
-// 
-void shiftBits(int DATA_pin, int CLOCK_pin, byte DATA_out) { 
-
-
-  pinMode(CLOCK_pin, OUTPUT);
-  pinMode(DATA_pin, OUTPUT);
-   // LATCH has already been set LOW prior to this call
-
-  // 0.b) Clear everything out just in case to prepare shift register for bit shifting
-  digitalWrite(DATA_pin, 0);  // DATA = 0 = LOW 
-  digitalWrite(CLOCK_pin, 0); // CLOCK = 0 = LOW 
-   // --[^BEFORE ALL]-----------------------------------------------
-
-
-     dataPinState = getDataPinState(); // Det. by what is on DigitalRead pin 
-
-    // 1.a) Then set DATA_pin HIGH(1) or LOW(0) depnding on resulting pinState 
-    digitalWrite(DATA_pin, dataPinState); // DATA = HIGH(1) OR LOW(0)
-
-
-     
-    // 1.b) REGISTER shifts bits on UPstroke (RISING edge) of CLOCK_pin  (THIS TRIGGERS SHIFT of E/ bit one position)
-    digitalWrite(CLOCK_pin, 1); // CLOCK = 1 = HIGH TO SHIFT BITS (adding what is on DATA pin(state) ^ above )
-     
-    // --[After EACH shift]------------------------------------------
-   // 3.b) ZERO the DATA_pin AFTER shift to prevent bleed through
-    digitalWrite(DATA_pin, 0); //DATA = 0 = LOW 
-  // LATCH will be set HIGH After this call 
-
-
-  // 9.) --[AFTER ALL (8) Shifts]------------------------------------------------
-  //STOP shifting
-  digitalWrite(CLOCK_pin, 0); // CLOCK BACK TO 0(LOW) to STOP Shifting 
-  // 9.b) LATCH will be shifted HIGH again to SHOW the bits on the LEDs
-}
-
-
-
-int getDataPinState(){
-  int rndPotVal = analogRead(RND_POT); // 10-bit resolution
-  bool currentOutBitVal; 
-  int randomNumber = random(0, 1023); 
-
-  if(BTN_HIGH == HIGH) { // 2
-    return 1;
-  } else if(BTN_LOW == HIGH) {
-    return 0;
-  }
-
-  // IF neither button is pressed the following code is executed: 
-  if (randomNumber >= rndPotVal) {
-     // then invert out shifted bit
-    currentOutBitVal  = !digitalRead(OUT_BIT_READ_PIN);
-  }
-  else {
-    // Leave the out shifted bit as it is.       
-    currentOutBitVal = digitalRead(OUT_BIT_READ_PIN);
-  }
-     
-  return (currentOutBitVal) ? 1 : 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-// PREVIOUS TODO: REMOVE
-void shiftOut(int DATA_pin, int CLOCK_pin, byte DATA_out) {
+void shiftBits(int DATA_pin, int CLOCK_pin, byte DATA_out)
+{
   // Shift 8 bits out MSB first, on clock's rising edge
-  // Had to add 10K PullDown resistor to clock input to keep LEDs from random flickers when clock cable is unplugged 
+  // Had to add 10K PullDown resistor to clock input to keep LEDs from random flickers when clock cable is unplugged
 
- 
- // Set CLOCK and DATA pins to OUTput
-  pinMode(CLOCK_pin, OUTPUT);
-  pinMode(DATA_pin, OUTPUT);
+  currentByte = DATA_out; // N
 
-  //clear everything out just in case to prepare shift register for bit shifting
-  digitalWrite(DATA_pin, 0);  // DATA = 0 = LOW 
-  digitalWrite(CLOCK_pin, 0); // CLOCK = 0 = LOW 
+  rndPotValue = analogRead(RND_POT); // Pin A1 (value will be 0 - 1023)
+  // nndPotValue = map(); // Would map 1023 to 255 but it seems to already by mapped to 255 (8-bit) by default (rather than 1023 (10-bit)
+  rndInt = random(0, 1024); // Random number from 0 to 1023
 
-  // Check and set e/ digit/bit space 1-by-1 and toggle it ON or OFF 
-  for (int i=7; i>=0; i--)  {
-    digitalWrite(CLOCK_pin, 0);// start [INTERNAL]CLOCK = 0 = LOW ..again (for e/iteration)
+  // Clear E/thing out, just in case, to prepare shift register for bit shifting
+  digitalWrite(DATA_pin, 0);  // DATA = 0 = LOW
+  digitalWrite(CLOCK_pin, 0); // CLOCK = 0 = LOW
 
-    // B/c they must ALL be set ONE-BY-ONE a bitmask is performed on e/ iteration. 
-    // Check bitmask result for this iteration (bit location)
-    if ( DATA_out & (1<<i) ) {  // any resulting # that is NOT 0 is TRUE
-      /*
-      Serial.print("[");
-      Serial.print(i);
-      Serial.print("]");
-      Serial.print("\t\t ++++++++++++++++++-> ");
-      Serial.println(DATA_out & (1<<i)); //128 64 _ 16 _ 4 _ _   I.e.  1000 000   0100 0000    0001 0000     0000 0100
-      Serial.print("\n");
-      */      
-      dataPinState= 1; // 1 = ON = HIGH
-    }
-    else {  // IF bitmask result is 0 theb ---> FALSE 
-      /*
-      Serial.print("[");
-      Serial.print(i);
-      Serial.print("]");
-      Serial.print("\t --------------------> ");
-      Serial.println(DATA_out & (1<<i)); //128 64 _ 16 _ 4 _ _   I.e.  1000 000   0100 0000    0001 0000     0000 0100
-      Serial.print("\n");`
-      */      
-      dataPinState= 0; // 0 = OFF = LOW 
-    }
+  digitalWrite(CLOCK_pin, 0); // start [INTERNAL]CLOCK = 0 = LOW ..again (for e/iteration)
 
-    // Then set DATA_pin HIGH(1) or LOW(0) depnding on resulting pinState 
-    digitalWrite(DATA_pin, dataPinState); // DATA = HIGH(1) OR LOW(0)  --> TO pin D4 Ard 
+  // I realize there is a chance these if-else statements may not be executed at the time
+  // a button is being held; HOWEVER, after testing this I found the behavior to be satisfactory.
+  // So, I decided not to add any additional interrupts to button presses etc. 
+  // Mostly, I didn't want to add this feature because the current behavior requiers 
+  // the musician/operator to press to the button(s) at the opportune moment in order to change the bit
+  // about to be carried over. If an interrupt occured whenever a button were pressed,
+  // it would likely introduce undesired bx; although, I've yet to test this. 
+  
+  // As you can see from the following statements, if both buttons are pressed at the same time, 
+  // by default, the carried bit is converted to 1 (not a 0).
+  if (digitalRead(BTN_HIGH) == HIGH)
+  { 
+    dataPinState = 1;
+    currentByte = (currentByte << 1) | (currentByte >> (NUMBER_OF_BITS_IN_SEQ - 1)) | 0b00000001; // | "Rotate through carry circular shift" (only unsigned so 0b11111111 = 129 [Not -127]).
+  }
+  else if (digitalRead(BTN_LOW) == HIGH)
+  { 
+    dataPinState = 0;
+    currentByte = ~(~(currentByte << 1) | (currentByte >> (NUMBER_OF_BITS_IN_SEQ - 1)) | 0b00000001);
+  }
+  else if (rndInt < rndPotValue)
+  { 
+    dataPinState = !digitalRead(OUT_BIT_READ_PIN); 
 
-    //Register shifts bits on UPstroke (RISING edge) of [INTERNAL]CLOCK_pin(D6)
-    // NOTE: EVERY time the clock goes from low to HIGH (EOR)ALL values currently stored in output registers are shifted over ONE position 
-    // The last one is discarded or ouput on the !Q_h pin
-    // SIMULTANEOUSLY, the value currently on the DATA input is shifted into the FIRST position (Q_a)
-    // Do this 8 TIMES to shift out all present values and shift IN values.
-    // LATCH is shifted HIGH at the end to make the new values appear on the outputs (LEDs).  
-    digitalWrite(CLOCK_pin, 1); // CLOCK = 1 = HIGH TO SHIFT BITS (adding what is on DATA pin(state) ^ above )
+    if (dataPinState)    
+      currentByte = (currentByte << 1) | (currentByte >> (NUMBER_OF_BITS_IN_SEQ - 1)) | 0b00000001;    
+    else     
+      currentByte = ~(~(currentByte << 1) | (currentByte >> (NUMBER_OF_BITS_IN_SEQ - 1)) | 0b00000001);    
+  }
+  else
+  {
+    dataPinState = digitalRead(OUT_BIT_READ_PIN);
+    currentByte = (currentByte << 1) | (currentByte >> (NUMBER_OF_BITS_IN_SEQ - 1)); // (sizeof(currentByte) - 1)  );
+  }
 
-    //ZERO the DATA_pin AFTER shift to prevent bleed through
-    digitalWrite(DATA_pin, 0); //DATA = 0 = LOW 
-  } //*********** close FOR-loop *********************************
+  digitalWrite(DATA_pin, dataPinState); // Set DATA pin (pin 4)to HIGH(1) OR LOW(0)  --> TO pin D4 Ard
+  digitalWrite(CLOCK_pin, 1);           // CLOCK = 1 = HIGH TO SHIFT BITS (adding what is on DATA pin(state) ^ above )
+  digitalWrite(DATA_pin, 0);            // DATA pin (Pin 4) = 0 = LOW
 
-
-  //STOP shifting
-  digitalWrite(CLOCK_pin, 0); // CLOCK BACK TO 0 to STOP Shifting 
+  // STOP shifting
+  digitalWrite(CLOCK_pin, 0); // CLOCK BACK TO 0 to STOP Shifting                           
+  // Serial.println("***********************");
+  // // Serial.print("["); Serial.print(currentIteration); Serial.print("]");
+  // Serial.print("Current Byte is : ");
+  // Serial.println(currentByte);
+  outputVoltage(currentByte);
 }
+
+// INTERRUPT f(n)
+void externalClkReceived()
+{
+  // ground LATCH_PIN and hold LOW for as long as transmitting
+  digitalWrite(LATCH_PIN, 0);
+
+  shiftBits(SER_DATA_PIN, CLK_PIN, currentByte); 
+                                                 // Return the latch pin HIGH to signal chip that it no longer needs to listen for information
+  digitalWrite(LATCH_PIN, 1);
+}
+
+// Called once to setup initial state from Setup()
+void firstShift(int DATA_pin, int CLOCK_pin, byte DATA_out)
+{
+  // Shift 8 bits out MSB first, on clock's rising edge.
+  // Clear everything out just in case to prepare shift register for bit shifting.
+  digitalWrite(DATA_pin, 0);  // DATA = 0 = LOW
+  digitalWrite(CLOCK_pin, 0); // CLOCK = 0 = LOW
+
+  // Check and set e/ digit/bit space 1-by-1 and toggle it ON or OFF
+  for (int i = 7; i >= 0; i--)
+  {
+    digitalWrite(CLOCK_pin, 0); // Start [INTERNAL]CLOCK = 0 = LOW ..again (for e/iteration).
+
+    if (DATA_out & (1 << i)) // Any resulting # that is NOT 0 ---> TRUE
+      dataPinState = 1;      // 1 = ON = HIGH
+    else                     // IF bitmask result is 0 then ---> FALSE
+      dataPinState = 0;      // 0 = OFF = LOW
+
+    digitalWrite(DATA_pin, dataPinState); // Set DATA pin (pin 4)to HIGH(1) OR LOW(0)  --> TO pin D4 Ard
+    digitalWrite(CLOCK_pin, 1);           // CLOCK = 1 = HIGH TO SHIFT BITS (adding what is on DATA pin(state) ^ above )
+    digitalWrite(DATA_pin, 0);            // DATA pin (Pin 4) = 0 = LOW
+  }                                       //*********** close FOR-loop *********************************
+  // STOP shifting
+  digitalWrite(CLOCK_pin, 0); // CLOCK BACK TO 0 to STOP Shifting
+
+  currentByte = DATA_out;
+  outputVoltage(currentByte);
+}
+
+// Called at the end of shiftBits()
+void outputVoltage(int val)
+{
+  // Start at 10 to avoid -neg voltages at low values (perhaps a rectifier diode instead but adds a voltage drop. A (buffered) OpAmp p(x)ly best.)
+  cvOutput = map(val, 0, 256, 5, 4095);
+  cvOutput = constrain(cvOutput, 0, 4095);
+  // Serial.print("[");
+  // Serial.print(val);
+  // Serial.print(" (10-bit value) maps to----> ");
+  // Serial.print(cvOutput);
+  // Serial.println(" (12-bit [mapped] Value)");
+  DAC.analogWrite(cvOutput);
+}
+
+/*
+outoutVoltage() results (recorded by serial outout and voltmeter[while using MCP4921 DAC with a max output of 5VDC]):
+  // 1 (10-bit value) maps to----> 15 (mapped to 12-bit) (---> 0.013v)
+  // 255 (10-bit) --maps to----> 4079 (12-bit) (---> ~4.945v)
+*/
+
+/*
+References:
+MCP4xxx family DAC info
+(from https://cyberblogspot.com/how-to-use-mcp4921-dac-with-arduino/#:~:text=The%20MCP4921%20is%20a%20Digital,the%20MCP4922%20is%20also%20available.    )
+*/
